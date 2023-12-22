818b0a26264725946f8893d309ea0b150cbe27f8
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-12-22 20:24:35 +0200

    diod

--- blinky.rs
+++ blinky.rs
@@ -82,12 +82,12 @@ fn main() -> ! {
     );
 
     // Configure GPIO25 as an output
-    let mut led_pin = pins.gpio25.into_push_pull_output();
+    let mut led_pin = pins.gpio15.into_push_pull_output();
     loop {
         led_pin.set_high().unwrap();
-        delay.delay_ms(500);
+        delay.delay_ms(100);
         led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        delay.delay_ms(100);
     }
 }
 
